f72a477556c876e2d609fcc1ab1c73d292e7bc4d
Gento <a551.return@gmail.com>
2021-03-07 13:58:30 +0900

    firestoreからtextを取得できた

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,25 +5,29 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('chats/xPF7trtQlVyMdsV8EIs7/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            //just add this line
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final docs = streamSnapshot.data.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text(docs[index]['text']),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/xPF7trtQlVyMdsV8EIs7/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((element) {
-              print(element['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
